8b13eceda33d413afa135dfb7aecd6f9c452ab6c
Benjamin Arias <benjamin.j.arias@gmail.com>
2016-08-25 12:45:45 +0200

    feat: Added labels to markers -- not working

--- components/controllers/map.ctr.js
+++ components/controllers/map.ctr.js
@@ -18,15 +18,30 @@
         },  
         marker2: {
             id: 2,
-            coords: { latitude: 52.518024, longitude: 13.384319 }
+            coords: { latitude: 52.518024, longitude: 13.384319 },
+            click: {},
+            options: {
+              label: "awesome",
+              animation: 1,
+            }
         },
         marker3: {
             id: 3,
-            coords: { latitude: 52.517444, longitude: 13.413050 }
+            coords: { latitude: 52.517444, longitude: 13.413050 },
+            click: {},
+            options: {
+              label: "awesome",
+              animation: 1,
+            }
         },     
         marker4: {
             id: 4,
-            coords: { latitude: 52.517444, longitude: 13.401050 }
+            coords: { latitude: 52.517444, longitude: 13.401050 },
+            click: {},
+            options: {
+              label: "awesome",
+              animation: 1,
+            }
         } 
       };   
     });
